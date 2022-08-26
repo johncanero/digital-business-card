ddcb571cde7a72fbec4784ad328ffad2b4be44d1
John Canero <johncanero.official@gmail.com>
2022-08-26 23:31:50 +0800

    Main Content Update

--- src/components/Main.js
+++ src/components/Main.js
@@ -4,8 +4,16 @@ const Main = () => {
   return (
     <div>
         <div className='digital--card'>
-             <a href="https://linktr.ee/johncanero" className="btn btn-light" type='button' role="button"><i class="fa-solid fa-arrow-up-right-from-square"></i> LinkedTree</a>
-            <a href="https://www.linkedin.com/in/johncanero/" className="btn btn-light" type='button' role="button"><i class="fa-brands fa-linkedin"></i> LinkedIn</a>
+             <a href="https://linktr.ee/johncanero" className="btn btn-light" type='button' role="button"><i class="fa-solid fa-arrow-up-right-from-square"></i> LinkTree</a>
+            <a href="https://www.linkedin.com/in/johncanero/" className="btn btn-light btn-color" type='button' role="button"><i class="fa-brands fa-linkedin"></i> LinkedIn</a>
+        </div>
+
+        <div className='digital--main'>
+          <h3>About</h3>
+          <p className='digital--text'>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s.</p>
+
+          <h3>Interests</h3>
+          <p className='digital--text'>It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters.</p>
         </div>
     </div>
   )
