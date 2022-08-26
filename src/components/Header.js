982bd22497bea05b8ccd91aba77f90b5db8ce196
John Canero <johncanero.official@gmail.com>
2022-08-26 20:50:26 +0800

    Hedaer Contents, Input Profile Photo and Styling

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,9 +1,17 @@
 import React from 'react'
+import PhotoCard from '../images/photoCard.jpg'
 
 const Header = () => {
   return (
     <div>
-    
+        <div className='card'>   
+            <div className='container'>
+                <img src={PhotoCard} alt='photoCard.jpg'/>
+                <h2>John Cañero</h2>
+                <h5>Frontend Developer</h5>
+                <p>johncanero.website</p>
+            </div>
+        </div>
     </div>
   )
 }
