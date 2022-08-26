90b7f8f1eaf5d768f8168009582c5f8bb5d0ab9c
John Canero <johncanero.official@gmail.com>
2022-08-27 00:14:49 +0800

    Anchor Tags link to Font Awesome Icons

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -4,10 +4,10 @@ const Footer = () => {
   return (
     <div>
         <footer className='footer-container'>
-            <i className="fa-brands fa-square-twitter fa-2x"></i>
-            <i class="fa-brands fa-square-facebook fa-2x"></i>
-            <i className="fa-brands fa-instagram fa-2x"></i>
-            <i className="fa-brands fa-square-github fa-2x"></i>
+            <a href="https://twitter.com/johncaneroo" target="_blank" rel="noopener noreferrer"><i className="fa-brands fa-square-twitter fa-2x"></i></a>
+            <a href="https://www.facebook.com/canerojohn/" target="_blank" rel="noopener noreferrer"><i class="fa-brands fa-square-facebook fa-2x"></i></a>
+            <a href="https://www.instagram.com/johncanero/" target="_blank" rel="noopener noreferrer"><i className="fa-brands fa-instagram fa-2x"></i></a>
+            <a href="https://github.com/johncanero" target="_blank" rel="noopener noreferrer"><i className="fa-brands fa-square-github fa-2x"></i></a>
         </footer>
     </div>
   )
