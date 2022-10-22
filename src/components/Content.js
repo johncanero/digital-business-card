d33b7a69bb66baade3b2b169b63c1c634ed09423
John Canero <johncanero.official@gmail.com>
2022-10-22 18:27:19 +0800

    Anchor Tag Decoration and Link Function

--- src/components/Content.js
+++ src/components/Content.js
@@ -1,23 +1,30 @@
-import React from 'react'
-import Main from './Main'
-import Footer from './Footer'
-import PhotoCard from '../images/sceenshots/photoCard.jpg'
+import React from "react";
+import Main from "./Main";
+import Footer from "./Footer";
+import PhotoCard from "../images/sceenshots/photoCard.jpg";
 
 const Content = () => {
   return (
-    <div className='digital--business-card'>
-        <div className='digital--card'>   
-            <div className='digital--container'>
-                <img src={PhotoCard} alt='photoCard.jpg'/>
-                <h1 className='header--name'>John Cañero</h1>
-                <h4 className='subheader--role'>Frontend Developer | Architecture Designer</h4>
-                <p className='subheader-website'>johncanero.website</p>
-            </div>
+    <div className="digital--business-card">
+      <div className="digital--card">
+        <div className="digital--container">
+          <img src={PhotoCard} alt="photoCard.jpg" />
+          <h1 className="header--name">John Cañero</h1>
+          <h4 className="subheader--role">
+            Frontend Developer | Architecture Designer
+          </h4>
+          <a 
+            href="https://johncanero.github.io/JamWebsite/" 
+            rel="noopener noreferrer"
+            target="_blank">
+          <p className="subheader-website">johncanero.website</p>
+          </a>
         </div>
-        <Main />
-        <Footer />
+      </div>
+      <Main />
+      <Footer />
     </div>
-  )
-}
+  );
+};
 
-export default Content
+export default Content;
